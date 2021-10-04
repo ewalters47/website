a9583381ffce9b8c7276353033f77b64932c5a1f
ewalters47 <emw45@students.calvin.edu>
2021-10-04 00:01:03 -0400

    Move default app function to index.js and render it

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import Navbar from './components/navbar/Navbar';
+import Navbar from './components/Navbar/Navbar';
 
-ReactDOM.render(<Navbar />, document.getElementById('root'));
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
