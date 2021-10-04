994a5fca501e190bbc50d3570c22a8ab79aa781a
ewalters47 <emw45@students.calvin.edu>
2021-10-03 22:07:15 -0400

    Initial commit

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,23 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: 'Experience',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: 'Homelab & Skills',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: 'Contact',
+        url: '#',
+        cName: 'nav-links',
+    },
+
+]
